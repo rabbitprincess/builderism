160f40ff259889274b7e36e87516f5a63a7f4a73
Chang <sjc02183@naver.com>
2024-03-19 21:21:05 +0900

    tidy code

--- init_node/2_init_environment.sh
+++ init_node/2_init_environment.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -eu
 
+L1_RPC_URL="$L1_RPC_URL"
+
 cd ~/optimism
 cp .envrc.example .envrc
 output=$(./packages/contracts-bedrock/scripts/getting-started/wallets.sh) \
@@ -13,8 +15,8 @@ output=$(./packages/contracts-bedrock/scripts/getting-started/wallets.sh) \
     -e 's/^export GS_PROPOSER_PRIVATE_KEY=.*/export GS_PROPOSER_PRIVATE_KEY='"$(echo "$output" | grep "GS_PROPOSER_PRIVATE_KEY" | cut -d'=' -f2-)"'/' \
     -e 's/^export GS_SEQUENCER_ADDRESS=.*/export GS_SEQUENCER_ADDRESS='"$(echo "$output" | grep "GS_SEQUENCER_ADDRESS" | cut -d'=' -f2-)"'/' \
     -e 's/^export GS_SEQUENCER_PRIVATE_KEY=.*/export GS_SEQUENCER_PRIVATE_KEY='"$(echo "$output" | grep "GS_SEQUENCER_PRIVATE_KEY" | cut -d'=' -f2-)"'/' \
-    -e '/^export L1_RPC_URL=/s@^export L1_RPC_URL=.*@export L1_RPC_URL=http://localhost:3000@' \
-    -e '/^export L1_RPC_KIND=/s@^export L1_RPC_KIND=.*@export L1_RPC_KIND=any@' \
+    -e 's/^export L1_RPC_URL=.*/export L1_RPC_URL='"$L1_RPC_URL"'/' \
+    -e 's/^export L1_RPC_KIND=.*/export L1_RPC_KIND=any/' \
 .envrc \
 && direnv allow && \
 eval "$(direnv hook bash)"
