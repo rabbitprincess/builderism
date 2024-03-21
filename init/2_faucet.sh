35dd75b172af85b6b5ac1a2e12d201b6f0965048
김창학 <sjc02183@naver.com>
2024-03-21 21:58:12 +0900

    .

--- init/2_faucet.sh
+++ init/2_faucet.sh
@@ -12,18 +12,19 @@ amount_eth="$2"
 # convert eth to wei
 amount_to_send=$(cast to-wei $amount_eth)
 
-# if sender have less eth than amount to send, error
-sender_balance=$(cast balance --address $FAUCET_ADDRESS --rpc-url $L1_RPC_URL)
-if [ $sender_balance -gt $amount_to_send ]; then
-  echo "$FAUCET_ADDRESS does not have enough balance"
-  exit 1
-fi
 
 # if receiver have more eth than amount to send, return
 receiver_balance=$(cast balance --address $receiver_address --rpc-url $L1_RPC_URL)
 if [ $receiver_balance -lt $amount_to_send ]; then
   echo "$receiver_address already has enough balance"
 else
+  # if sender have less eth than amount to send, error
+  sender_balance=$(cast balance --address $FAUCET_ADDRESS --rpc-url $L1_RPC_URL)
+  if [ $sender_balance -gt $amount_to_send ]; then
+    echo "$FAUCET_ADDRESS does not have enough balance"
+    exit 1
+  fi
+
   # send eth
   cast send --to $receiver_address --value $amount_to_send --from $FAUCET_ADDRESS --private-key $FAUCET_PRIVATE_KEY --rpc-url $L1_RPC_URL
 fi
