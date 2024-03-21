ff9080c44fe880f48b9ec1705913a5b3aa40df3d
김창학 <sjc02183@naver.com>
2024-03-21 22:09:44 +0900

    fix forge commands

--- init/2_faucet.sh
+++ init/2_faucet.sh
@@ -13,17 +13,17 @@ amount_eth="$2"
 amount_to_send=$(cast to-wei $amount_eth)
 
 # if receiver have more eth than amount to send, no need to send
-receiver_balance=$(cast balance --address $receiver_address --rpc-url $L1_RPC_URL)
+receiver_balance=$(cast balance $receiver_address --rpc-url $L1_RPC_URL)
 if [ $receiver_balance -lt $amount_to_send ]; then
   echo "$receiver_address already has enough balance"
 else
   # if sender have less eth than amount to send, error
-  sender_balance=$(cast balance --address $FAUCET_ADDRESS --rpc-url $L1_RPC_URL)
+  sender_balance=$(cast balance $FAUCET_ADDRESS --rpc-url $L1_RPC_URL)
   if [ $sender_balance -gt $amount_to_send ]; then
     echo "$FAUCET_ADDRESS does not have enough balance"
     exit 1
   fi
 
   # send eth
-  cast send --to $receiver_address --value $amount_to_send --from $FAUCET_ADDRESS --private-key $FAUCET_PRIVATE_KEY --rpc-url $L1_RPC_URL
+  cast send $receiver_address --value $amount_to_send --from $FAUCET_ADDRESS --private-key $FAUCET_PRIVATE_KEY --rpc-url $L1_RPC_URL
 fi
