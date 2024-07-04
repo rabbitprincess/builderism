600d4f4d4ff012f4105957aa911308d905be4b2c
Kim <dreamcacao02183@gmail.com>
2024-07-04 15:49:43 +0900

    update op version 1.7.3 to 1.7.6

--- init/5_export_genesis.sh
+++ init/5_export_genesis.sh
@@ -3,16 +3,15 @@ set -eu
 
 echo "[5/5] : export genesis files"
 
-env_file="/config/address.ini"
-
 cd ~/optimism/op-node
 go run cmd/main.go genesis l2 \
   --deploy-config ../packages/contracts-bedrock/deploy-config/$DEPLOYMENT_CONTEXT.json \
   --l1-deployments ../packages/contracts-bedrock/deployments/$DEPLOYMENT_CONTEXT/.deploy \
   --outfile.l2 /config/genesis.json \
   --outfile.rollup /config/rollup.json \
-  --l1-rpc $L1_RPC_URL
+  --l1-rpc $L1_RPC_URL \
+  --l2-allocs ../packages/contracts-bedrock/deployments/state-dump-$DEPLOYMENT_CONTEXT.json
 
-cp ~/optimism/packages/contracts-bedrock/deployments/$DEPLOYMENT_CONTEXT/.deploy /config/.deploy
+cp ../packages/contracts-bedrock/deployments/$DEPLOYMENT_CONTEXT/.deploy /config/.deploy
 
 echo "[all process is done! check config files.]"
