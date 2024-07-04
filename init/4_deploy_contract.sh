b9b53f5a223f1051315142f135364919fc1eb174
Kim <dreamcacao02183@gmail.com>
2024-07-04 19:42:21 +0900

    fix deployment paths

--- init/4_deploy_contract.sh
+++ init/4_deploy_contract.sh
@@ -3,21 +3,21 @@ set -eu
 
 echo "[4/5] : deploy contract"
 
-export DEPLOY_CONFIG_PATH="~/optimism/packages/contracts-bedrock/deploy-config/87655678.json"
-export CONTRACT_ADDRESSES_PATH="~/optimism/packages/contracts-bedrock/deployments/$L1_CHAIN_ID-deploy.json"
-export STATE_DUMP_PATH="~/optimism/packages/contracts-bedrock/deployments/"
-
 cd ~/optimism/packages/contracts-bedrock && \
+DEPLOY_CONFIG_PATH="./deploy-config/$DEPLOYMENT_CONTEXT.json" \
 forge script scripts/Deploy.s.sol:Deploy \
   --private-key $ADMIN_PRIVATE_KEY \
   --broadcast \
   --rpc-url $L1_RPC_URL \
   --priority-gas-price $PRIORITY_GAS_PRICE \
-  --slow && \
+  --slow
+
+CONTRACT_ADDRESSES_PATH="./deployments/$DEPLOYMENT_CONTEXT/deploy.json" \
+DEPLOY_CONFIG_PATH="./deploy-config/$DEPLOYMENT_CONTEXT.json" \
 forge script scripts/L2Genesis.s.sol:L2Genesis \
   --sig 'runWithAllUpgrades()' \
   --rpc-url $L1_RPC_URL
 
-# mv state-dump-$DEPLOYMENT_CONTEXT.json deployments/
-# mv state-dump-$DEPLOYMENT_CONTEXT-delta.json deployments/
-# mv state-dump-$DEPLOYMENT_CONTEXT-ecotone.json deployments/
+mv ./state-dump-$DEPLOYMENT_CONTEXT.json ./deployments
+mv ./state-dump-$DEPLOYMENT_CONTEXT-delta.json ./deployments
+mv ./state-dump-$DEPLOYMENT_CONTEXT-ecotone.json ./deployments
--- init/5_export_genesis.sh
+++ init/5_export_genesis.sh
@@ -6,12 +6,12 @@ echo "[5/5] : export genesis files"
 cd ~/optimism/op-node
 go run cmd/main.go genesis l2 \
   --deploy-config ../packages/contracts-bedrock/deploy-config/$DEPLOYMENT_CONTEXT.json \
-  --l1-deployments ../packages/contracts-bedrock/deployments/$DEPLOYMENT_CONTEXT/.deploy \
+  --l1-deployments ../packages/contracts-bedrock/deployments/$DEPLOYMENT_CONTEXT/deploy.json \
   --outfile.l2 /config/genesis.json \
   --outfile.rollup /config/rollup.json \
   --l1-rpc $L1_RPC_URL \
   --l2-allocs ../packages/contracts-bedrock/deployments/state-dump-$DEPLOYMENT_CONTEXT.json
 
-cp ../packages/contracts-bedrock/deployments/$DEPLOYMENT_CONTEXT/.deploy /config/.deploy
+cp ../packages/contracts-bedrock/deployments/$DEPLOYMENT_CONTEXT/deploy.json /config/deploy.json
 
 echo "[all process is done! check config files.]"
