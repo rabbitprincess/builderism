43a00509f8bfe9aa81152509514b0c1a0cf770e2
rabbitprincess <sjc02183@naver.com>
2024-07-06 13:46:18 +0900

    fix foundry and fork version fjord not implemented yet

--- init/5_export_genesis.sh
+++ init/5_export_genesis.sh
@@ -10,7 +10,7 @@ go run cmd/main.go genesis l2 \
   --outfile.l2 /config/genesis.json \
   --outfile.rollup /config/rollup.json \
   --l1-rpc $L1_RPC_URL \
-  --l2-allocs ../packages/contracts-bedrock/state-dump-$DEPLOYMENT_CONTEXT.json
+  --l2-allocs ../packages/contracts-bedrock/state-dump-$DEPLOYMENT_CONTEXT-ecotone.json
 
 cp ../packages/contracts-bedrock/deployments/$L1_CHAIN_ID-deploy.json /config/deploy.json
 
