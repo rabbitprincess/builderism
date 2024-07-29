7a16593d151bbedfb9d9e49a1c59c2248928a5b7
rabbitprincess <sjc02183@naver.com>
2024-07-29 17:06:47 +0900

    .

--- init/4_deploy_contract.sh
+++ init/4_deploy_contract.sh
@@ -13,21 +13,6 @@ forge script scripts/deploy/Deploy.s.sol:Deploy \
 
 echo "[deploy contract done!]"
 
-#!/bin/bash
-set -eu
-
-echo "[4/5] : deploy contract"
-
-cd ~/optimism/packages/contracts-bedrock && \
-DEPLOY_CONFIG_PATH="./deployments/$DEPLOYMENT_CONTEXT/.deploy" \
-forge script scripts/deploy/Deploy.s.sol:Deploy \
-  --private-key $ADMIN_PRIVATE_KEY \
-  --broadcast \
-  --rpc-url $L1_RPC_URL \
-  --priority-gas-price $PRIORITY_GAS_PRICE
-
-echo "[deploy contract done!]"
-
 CONTRACT_ADDRESSES_PATH="./deployments/$L1_CHAIN_ID-deploy.json" \
 DEPLOY_CONFIG_PATH="./deployments/$DEPLOYMENT_CONTEXT/.deploy" \
 FORK="fjord" \
