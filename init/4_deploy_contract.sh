2945b32c8c147d7d4a3bdabb44e5df68d43db0fb
rabbitprincess <sjc02183@naver.com>
2024-07-29 17:05:58 +0900

    generate only fjord allocs

--- init/4_deploy_contract.sh
+++ init/4_deploy_contract.sh
@@ -13,9 +13,24 @@ forge script scripts/deploy/Deploy.s.sol:Deploy \
 
 echo "[deploy contract done!]"
 
+#!/bin/bash
+set -eu
+
+echo "[4/5] : deploy contract"
+
+cd ~/optimism/packages/contracts-bedrock && \
+DEPLOY_CONFIG_PATH="./deployments/$DEPLOYMENT_CONTEXT/.deploy" \
+forge script scripts/deploy/Deploy.s.sol:Deploy \
+  --private-key $ADMIN_PRIVATE_KEY \
+  --broadcast \
+  --rpc-url $L1_RPC_URL \
+  --priority-gas-price $PRIORITY_GAS_PRICE
+
+echo "[deploy contract done!]"
+
 CONTRACT_ADDRESSES_PATH="./deployments/$L1_CHAIN_ID-deploy.json" \
 DEPLOY_CONFIG_PATH="./deployments/$DEPLOYMENT_CONTEXT/.deploy" \
+FORK="fjord" \
 forge script scripts/L2Genesis.s.sol:L2Genesis \
-  --sig 'runWithAllUpgrades()' \
-  --chain-id $L2_CHAIN_ID \
-  --rpc-url $L1_RPC_URL \
+  --sig 'runWithStateDump()' \
+  --rpc-url $L1_RPC_URL
