edac66ca55a7689115ab2df9aa079f5c8c1d6dbf
Kim <dreamcacao02183@gmail.com>
2024-09-26 12:17:56 +0900

    tidy configs

--- scan/indexer-entrypoint.sh
+++ scan/indexer-entrypoint.sh
@@ -4,6 +4,4 @@ set -eu
 cd /app/op-scan && cp /config/scan.env .env.local
 export $(grep -v '^#' .env.local | xargs)
 
-pnpm prisma:db:push
-
-pnpm op-indexer
+pnpm prisma:db:push && pnpm op-indexer
