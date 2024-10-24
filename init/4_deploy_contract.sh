993b16963f6b844ef3aea8bb97c689d6894f3f10
Kim <dreamcacao02183@gmail.com>
2024-10-24 18:09:21 +0900

    set version op-deployer

--- init/4_deploy_contract.sh
+++ init/4_deploy_contract.sh
@@ -5,7 +5,7 @@ echo "[4/5] : deploy contract"
 
 cd ~/optimism/packages/contracts-bedrock && \
 DEPLOY_CONFIG_PATH="./deployments/$DEPLOYMENT_CONTEXT/.deploy" \
-forge script scripts/deploy/Deploy.s.sol:Deploy \
+forge script -vvv scripts/deploy/Deploy.s.sol:Deploy \
   --private-key $ADMIN_PRIVATE_KEY \
   --broadcast \
   --rpc-url $L1_RPC_URL \
@@ -16,6 +16,6 @@ echo "[deploy contract done, start to deploy L2Genesis]"
 CONTRACT_ADDRESSES_PATH="./deployments/$L1_CHAIN_ID-deploy.json" \
 DEPLOY_CONFIG_PATH="./deployments/$DEPLOYMENT_CONTEXT/.deploy" \
 FORK="granite" \
-forge script scripts/L2Genesis.s.sol:L2Genesis \
+forge script -vvv scripts/L2Genesis.s.sol:L2Genesis \
   --sig 'runWithStateDump()' \
   --rpc-url $L1_RPC_URL
